e2fbab7239e3e547a839492eae1603a4eb0e0312
Stephane Loppinet <stephaneloppinet@gmail.com>
2025-10-07 09:17:44 +0200

    init backend with one simple route

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Corto-web
+
+## Start the application
+- Define the .env variables (check the keys in the .env-example file)
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+
+app.get("/", (_, response) =>
+  response.json({ info: "Express app with Supabase" })
+);
+
+app.listen(process.env.PORT, () =>
+  console.log(
+    new Date().toLocaleTimeString() +
+      `: Server is running on port ${process.env.PORT}...`
+  )
+);
